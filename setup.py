b1462d21c0066fe6b16507cec1a267c81cacb526
levisstrauss <levisstrauss11@yahoo.com>
2026-01-03 22:15:25 -0500

    Final version

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import setuptools
 
 setuptools.setup(
-    name="starter",
-    version="0.0.0",
-    description="Starter code.",
-    author="Student",
+    name="census-income-classifier",
+    version="1.0.0",
+    description="Census Income Classification ML Pipeline with FastAPI deployment",
+    author="Codemon",
+    packages=setuptools.find_packages(),
+    python_requires="3.11",
 )
